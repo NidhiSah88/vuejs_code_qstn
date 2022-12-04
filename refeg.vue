7eebd64f1d6a012be258751c66529e87691cae29
NidhiSah88 <90081445+NidhiSah88@users.noreply.github.com>
2022-12-04 18:31:19 +0530

    Add files via upload

---
+++ refeg.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <div>
+      <h3>{{counterTitle}} </h3>
+      
+    </div>
+    <div>
+      <h4> Edit title:</h4>
+      <input type="text" v-model="counterTitle" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+  import {ref} from 'vue'
+
+const counterTitle = ref('My counter')
+
+   
+
+</script>
